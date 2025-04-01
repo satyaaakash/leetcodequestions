f9383584b4b63a6eb52f3966d0fdec204cba88bf
Satya Aakash Obellaneni <akashchowdary500@gmail.com>
2025-04-01 19:10:09 -0400

    [105] [Time Beats: 47.98%] [Memory Beats: 35.34%] - LeetPush

--- Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py
+++ Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py
@@ -6,13 +6,25 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        #if preorder or inorder is empty we return none that is the base condition for this question
+
         if not preorder or not inorder:
             return None
-        
+
+        #other than this , first we should create root node for tree with preorder first element
+
         root = TreeNode(preorder[0])
+
+        #now we should assign mid as from inorder , we can saythat before mid was left subtree and after mid was right subtree
         mid = inorder.index(preorder[0])
 
+        #define root.left for building left subtree
         root.left = self.buildTree(preorder[1:mid+1],inorder[:mid])
+
+        #same way define root.right
+
         root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:])
-        return root        
+
+        #finally return root 
+        return root
         
