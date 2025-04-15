ebc4650095f1196365d227c5ba5cccf9b4b66235
Satya Aakash Obellaneni <akashchowdary500@gmail.com>
2025-04-15 01:38:10 -0400

    [46] [Time Beats: 100.00%] [Memory Beats: 80.70%] - LeetPush

--- Permutations.py
+++ Permutations.py
@@ -3,13 +3,20 @@ class Solution:
         result =[]
 
         def backtrack(start):
+            #base case : when start completes the iteration through the nums , need to append that to result
             if len(nums)==start:
                 result.append(nums[:])
                 return
+            #recursive case
             for i in range(start,len(nums)):
+                #swap ith element and start element (make that choice)
                 nums[start],nums[i]=nums[i],nums[start]
+                #backtrack it after swapping and complete it
                 backtrack(start+1)
+                #undo the choice was made , so swap again
                 nums[start],nums[i]=nums[i],nums[start]
+        #call first element from nums to start the process
         backtrack(0)
+        #return the result
         return result
         
