7bd6d934eeb28163c41c4573e78473f1f3b0ff44
Satya Aakash Obellaneni <akashchowdary500@gmail.com>
2025-04-18 04:37:49 -0400

    [200] [Time Beats: 92.27%] [Memory Beats: 82.90%] - LeetPush

---
+++ Number-of-Islands.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        if not grid:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+
+        visited = [[False]*cols for _ in range(rows)]
+
+        def dfs(r,c):
+
+            if r<0 or r>=rows or c<0 or c>=cols or grid[r][c]==\0\ or visited[r][c]:
+                return
+            
+            visited[r][c]=True
+
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        
+
+        islands = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]==\1\ and not visited[r][c]:
+                    dfs(r,c)
+                    islands+=1
+        return islands
+        
