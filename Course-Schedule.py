39f4238d3029ffb05943260a4eaccbcd617e7e8e
Satya Aakash Obellaneni <akashchowdary500@gmail.com>
2025-05-07 19:24:08 -0400

    [207] [Time Beats: 47.76%] [Memory Beats: 98.41%] - LeetPush

--- Course-Schedule.py
+++ Course-Schedule.py
@@ -8,8 +8,8 @@ class Solution:
         
         indegree = [0]*numCourses
         for a,b in prerequisites:
-            graph[a].append(b)
-            indegree[b] +=1
+            graph[b].append(a)
+            indegree[a] +=1
 
         queue = deque()
 
