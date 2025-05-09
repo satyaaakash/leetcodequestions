f296f1c8fed9c63fe2df694317656ff3d1fe084a
Satya Aakash Obellaneni <akashchowdary500@gmail.com>
2025-05-08 21:58:35 -0400

    [743] [Time Beats: 90.56%] [Memory Beats: 98.57%] - LeetPush

--- Network-Delay-Time.py
+++ Network-Delay-Time.py
@@ -8,18 +8,20 @@ class Solution:
         for u,v,w in times:
             graph[u].append((v,w)) #here u i.e source node acts as key and v i.e target node and w which is distance/time that needs to travel acts as value 
         
-        min_times = {}  # initiaLIZES dictionary to store the minimum
-        min_heap = [(0,k)] 
-        while min_heap:
-            time, node = heapq.heappop(min_heap)
+        min_times = {}  # initializes dictionary to store the minimum
+        min_heap = [(0,k)] #initialize min heaop with 0 as distance/time and k as given node 
+        while min_heap: #till min_heap gets empty
+            time, node = heapq.heappop(min_heap) # pop out the shortest distance in the heap
 
-            if node in min_times:
+            # if node was alrready min_heap then continue , if not then assign time to that node in hashmap to permenanetly store that the value(time/weright) will be minimum for that node 
+
+            if node in min_times: 
                 continue
             min_times[node] = time
 
-            for neighbor, neighbor_time in graph[node]:
-                if neighbor not in min_times:
-                    heapq.heappush(min_heap,(time+neighbor_time,neighbor))
+            for neighbor, neighbor_time in graph[node]:  #traverser through all available neighbors to thast node
+                if neighbor not in min_times: # if neighbor was not in hashmap 
+                    heapq.heappush(min_heap,(time+neighbor_time,neighbor)) #then push that time + neighbor time with that neighbor node to min_heap 
         if len(min_times)==n:
             return max(min_times.values())
         return -1
