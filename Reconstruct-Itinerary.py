3abd7f667cf446ade20ccab1e2de7776ecfeaf2a
Satya Aakash Obellaneni <akashchowdary500@gmail.com>
2025-05-11 12:19:18 -0400

    [332] [Time Beats: 75.29%] [Memory Beats: 17.74%] - LeetPush

--- Reconstruct-Itinerary.py
+++ Reconstruct-Itinerary.py
@@ -3,22 +3,38 @@ class Solution:
 
         graph = defaultdict(list)
 
-        for source, destination in sorted(tickets, reverse = True):
-            graph[source].append(destination)
+        for src, dst in tickets:
+            heapq.heappush(graph[src],dst)
+        result =[]
+
+        def dfs(source):
+            while graph[source]:
+                dfs(heapq.heappop(graph[source]))
+            result.append(source)
         
-        stack = ["JFK"]
-        iternary = []
-
-        while stack:
-            current = stack[-1]
-
-            if not graph[current]:
-                iternary.append(stack.pop())
-            else:
-                next_city = graph[current].pop()
-                stack.append(next_city)
-
-        return iternary[::-1]
+        dfs("JFK")
+        return result[::-1]
 
 
+
+        # graph = defaultdict(list)
+
+        # for source, destination in sorted(tickets, reverse = True):
+        #     graph[source].append(destination)
         
+        # stack = ["JFK"]
+        # iternary = []
+
+        # while stack:
+        #     current = stack[-1]
+
+        #     if not graph[current]:
+        #         iternary.append(stack.pop())
+        #     else:
+        #         next_city = graph[current].pop()
+        #         stack.append(next_city)
+
+        # return iternary[::-1]
+
+
+        #time complexity same for both approaches (O(ElogE)), space complexity is bettern in sorted reverse approach
