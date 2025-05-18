3cefab4311655bd3bd29a33687e7270fbfb91eb3
Satya Aakash Obellaneni <akashchowdary500@gmail.com>
2025-05-18 15:28:58 -0400

    [322] [Time Beats: 64.94%] [Memory Beats: 43.21%] - LeetPush

--- Coin-Change.py
+++ Coin-Change.py
@@ -1,41 +1,36 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         #bottom - up approach , time : O(amount *coins), space : O(amount)
-        coins.sort()
-        dp = [0]*(amount+1)
+        
+        dp = [amount+1]*(amount+1)
+        dp[0] = 0
 
         for i in range(1,amount+1):
-            minimum = float('inf')
             for coin in coins:
-                diff = i- coin
-                if diff <0:
-                    break
-                minimum = min(minimum,dp[diff]+1)
-            dp[i] = minimum
-        if dp[amount]<float('inf'):
-            return dp[amount] 
+                if i-coin>=0:
+                    dp[i] = min(dp[i],1+dp[i-coin])
+        if dp[amount]!= (amount+1):
+            return dp[amount]
         else:
-            return -1       
+            return -1
+        
+        #top-down approach , time : O(amount*coins), space : O(amount)
 
-        #top-down approach , time and space as bottom up , but btoom up is faster than this
-        # coins.sort()
-        # memo ={0:0}
+        # memo = {}
 
-        # def dfs(amt):
-
-        #     minimum = float('inf')
-
-        #     if amt in memo:
-        #         return memo[amt]
+        # def helper(amount):
+        #     if amount ==0 :
+        #         return 0
+        #     if amount in memo:
+        #         return memo[amount]
             
-        #     for coin in coins :
-        #         diff = amt - coin
-        #         if diff<0:
-        #             break
-        #         minimum = min(minimum,dfs(diff)+1)
-        #     memo[amt] = minimum
-        #     return minimum
-        # if dfs(amount)<float('inf'):
-        #     return dfs(amount)
-        # else:
-        #     return -1
+        #     result = float('inf')
+
+        #     for coin in coins:
+        #         if amount - coin >=0:
+        #             result = min(result, 1+helper(amount-coin))
+        #     memo[amount] = result
+        #     return result 
+        # mincoins = helper(amount)
+        # return mincoins if mincoins!=float('inf') else -1
+            
