2556c18ce5e775c3c374d5b29844e4ac6826a436
Satya Aakash Obellaneni <akashchowdary500@gmail.com>
2025-05-22 17:36:09 -0400

    [846] [Time Beats: 59.01%] [Memory Beats: 45.02%] - LeetPush

---
+++ Hand-of-Straights.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        #time:O(nlogn), space:O(n)
+        if len(hand)%groupSize !=0:
+            return False
+        
+        count ={}
+
+        for i in hand:
+            count[i]=1+count.get(i,0)
+
+        min_heap = list(count.keys())
+        heapq.heapify(min_heap)
+
+        while min_heap:
+            first = min_heap[0]
+
+            for i in range(first,first+groupSize):
+                if i not in count:
+                    return False
+                
+                count[i]-=1
+                if count[i]==0:
+                    if i!=min_heap[0]:
+                        return False
+                    heapq.heappop(min_heap)
+        return True
+
+        
