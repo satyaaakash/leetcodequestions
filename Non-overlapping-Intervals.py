88e1e28c53a821ea741ac80aefc92f50efe1be4a
Satya Aakash Obellaneni <akashchowdary500@gmail.com>
2025-05-26 14:00:42 -0400

    [435] [Time Beats: 96.45%] [Memory Beats: 22.20%] - LeetPush

--- Non-overlapping-Intervals.py
+++ Non-overlapping-Intervals.py
@@ -1,6 +1,7 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         #time :O(nlogn), space:O(1)
+        #approach : We pick as many non-overlapping intervals as possible, and remove the rest.
 
         intervals.sort(key=lambda interval:interval[1])
         result = intervals[0][1]
